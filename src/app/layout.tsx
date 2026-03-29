d4157d1f503adadfa71ec5d3907080b34fb55d48
Manoo089 <manoo089@googlemail.com>
2026-03-29 18:00:37 +0200

    feat: add vercels analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Syne, DM_Mono } from "next/font/google";
 import "../styles/globals.scss";
 import Navigation from "@/components/Navigation/Navigation";
@@ -58,6 +59,7 @@ export default function RootLayout({
         </header>
         {children}
         <ScrollToTop />
+        <Analytics />
       </body>
     </html>
   );
